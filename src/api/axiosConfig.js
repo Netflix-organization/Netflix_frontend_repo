1805521864910853bc36bafe070d1219e542e4c4
Stella <stellajoel247@gmail.com>
2026-04-27 15:52:29 +0100

    Update axios base URL to new server address

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.15.194.21:8080',
+    baseURL:'http://13.247.214.191:8080',
     headers: {
         'Content-Type': 'application/json',
     },
